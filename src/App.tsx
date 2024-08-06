56e4262aa83539d2a0c1e821b189634a2a2269a2
Shubhaw <shubhawkumar@gmail.com>
2024-08-06 10:57:48 +0530

    basic requirements done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import { ProgressBar } from "./components/ProgressBar";
 
+const MIN_PROGRESS = 0;
+const MAX_PROGRESS = 100;
+const STEP = 4;
+const INCREMENT_INTERVAL = 70;
+
 function App() {
-  return <ProgressBar />;
+  const [currentProgress, setCurrentProgress] = useState(MIN_PROGRESS);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentProgress((prevProgress) => {
+        if (prevProgress >= MAX_PROGRESS) {
+          clearInterval(interval);
+        }
+        return Math.min(prevProgress + STEP, MAX_PROGRESS);
+      });
+    }, INCREMENT_INTERVAL);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  return <ProgressBar progress={currentProgress} progressColor="yellow" width="300px" />;
 }
 
 export default App;
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,3 +1,24 @@
-export const ProgressBar = () => {
-  return <h1>Progress Bar</h1>;
+import "./ProgressBar.style.css";
+
+interface IProgressBarProps {
+  width?: string;
+  height?: string;
+  backgroundColor?: string;
+  progressColor?: string;
+  progress?: number;
+}
+
+export const ProgressBar = (props: IProgressBarProps) => {
+  const { width = "500px", backgroundColor = "#505050", progressColor = "green", progress = 10 } = props;
+  return (
+    <>
+      <div className="container" style={{ width, backgroundColor }}>
+        <span className="progress-indicator">{`${progress}%`}</span>
+        <div
+          className="progress"
+          style={{ backgroundColor: progressColor, transform: `translate(${progress - 100}%)` }}
+        ></div>
+      </div>
+    </>
+  );
 };
